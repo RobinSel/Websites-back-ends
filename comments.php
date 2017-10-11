3ecc0fadaa19a0d8862827a4dbb6d8188c6e172b
RobinSel <robin.sel@student.kdg.be>
2017-10-11 11:19:42 +0200

    opdacht comments

---
+++ comments.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php 
+    
+    echo "naam: Robin Sel";
+    
+    /*
+    naam: Robin Sel
+    e-mail: robin.sel@student.kdg.be
+    */   
+    
+    ?>
+    
+</body>
+</html>
