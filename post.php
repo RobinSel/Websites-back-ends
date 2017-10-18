c51a010840df5a8cd65fa2d4ef8aa57b4551c4f1
RobinSel <robin.sel@student.kdg.be>
2017-10-18 12:09:26 +0200

    opdracht post

---
+++ post.php
@@ -0,0 +1,50 @@
+<?php
+$username = "Peter";
+$password = "Tak123tak";
+$message = "Welkom";
+
+if (isset ($_POST["submit"])) {
+    if ($_POST["Username"] == $username && $_POST["paswoord"] == $password){
+        echo $message;
+    }
+    else {
+        echo "inloggen mislukt!";
+    }
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    
+    <style>
+    
+        fieldset {
+            font-weight: bold;
+        }   
+    
+    </style>
+    
+</head>
+<body>
+    
+    <form action="post.php" method="post">
+        <h3>Log in</h3><br>
+        <label for="name">Gebruikersnaam: </label>
+        <input type="text" name="Username" id="Username"> <br>
+        <label for="password">Wachtwoord: </label>
+        <input type="password" name="paswoord" id="paswoord"><br>
+        <input type="submit" name="submit">
+        
+    </form>
+    
+    
+    
+    
+    
+    
+</body>
+</html>
