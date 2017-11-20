53f0f43b4f9ee558b67eab49190ab6311dae781e
RobinSel <robin.sel@student.kdg.be>
2017-11-20 17:35:08 +0100

    opdracht class begin
    Werkt nog niet helemaal

---
+++ opdracht-classes-begin/application.php
@@ -0,0 +1,45 @@
+<?php
+require_once 'classes/percent.php';
+
+$test = new Percent(150, 100);
+
+$relative = $test->relative;
+$hundred = $test->hundred;
+$nominal = $test->nominal;
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <style>
+        table, td{
+            border: 1px solid #000;
+        }
+        table{
+            border-collapse: collapse;
+        }
+        td{
+            padding: 20px;
+        }
+    </style>
+</head>
+<body>
+    <p>Hoe staat 150 in <br> verhouding tot 100?</p>
+    <table>
+        <tr>
+            <td>Relatief</td>
+            <td><?php $relative ?></td>
+        </tr>
+        <tr>
+            <td>Procentueel</td>
+            <td><?php $hundred ?></td>
+        </tr>
+        <tr>
+            <td>Nominaal</td>
+            <td><?php $nominal ?></td>
+        </tr>
+        
+    </table>
+</body>
+</html>
---
+++ opdracht-classes-begin/classes/percent.php
@@ -0,0 +1,26 @@
+<?php
+class Percent{
+    
+    public $relative;
+    public $hundred;
+    public $nominal;
+    
+    public function __construct ($new, $unit) {
+        $this->relative = $new/$unit;
+        $this->hundred = $this->relative * 100;
+        
+        if ($this->relative > 1){$this->nominal = "Positive";}
+        if ($this->relative = 1){$this->nominal = "Status-quo";}
+        if ($this->relative < 1){$this->nominal = "Negative";}
+        }
+    public function roundNumber($number) {
+        $roundedNumber = number_format($number);
+        return $roundedNumber;
+    }    
+    
+    
+}
+
+
+
+?>
