cc5efe5353301b60d38b16bfc96c99a49d387e14
RobinSel <robin.sel@student.kdg.be>
2017-11-20 11:09:58 +0100

    Samenvoeging van html in php

---
+++ opdracht-include-require/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php
+    include "view/header-partial.html";
+    include "view/body-partial.html";
+    include "view/footer-partial.html";
+    ?>
+    
+</body>
+</html>
