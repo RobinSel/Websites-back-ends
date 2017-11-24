b15aefcfd208028e7623a0c3cc51cdfaae3d79ef
RobinSel <robin.sel@student.kdg.be>
2017-11-24 11:34:21 +0100

    opdracht error-handling

--- opdracht-error-handling/error-handling.php
+++ opdracht-error-handling/error-handling.php
@@ -1,16 +1,33 @@
 <?php
 
+$kortingscode =	'';
+$message = false;
+
 try
     {
 		if ( isset ( $_POST['submit'] ) ) {
-            if ($_POST['kortingscode'] == null) {
-               throw new Exception ();
+            if ($_POST['kortingscode'] == '') {
+               throw new Exception ('Geen kortingscode gegeven!');
+            }
+            if (strlen($_POST['kortingscode']) <8) {
+                throw new Exception ('Kortingscode is niet geldig!');
             }
 		}
 	}
 catch (exception $e)
     {
-        
+        $message['type'] = 'error';
+		$message['text'] = $e->getMessage();
+
+		$date	=	'[' . date( 'H:i:s d/m/Y', time() ).']';
+        $ipAdress = $_SERVER['REMOTE_ADDR'];
+
+        // bv: [11:12:53 08/08/2015] - 127.0.0.1 - type:[error] Kortingscode is niet correct
+		$errorString	=	$date . ' - ' . $ipAdress . ' - ' . "type:[" . $message['type'] . "] " . $message['text'] . "\n\r";
+
+		file_put_contents( 'error.log', $errorString, FILE_APPEND );
+    
+
     }
 
 ?>
